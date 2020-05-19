f8f115090492b702257462a8dd550dac7c690dc8
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-05-19 19:12:57 +0100

    Add markdown preview output

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,19 @@ async function run(): Promise<void> {
 
     await exec.exec('terraform', ['plan', '-input=false', `-out=${out}`], { cwd })
 
+    let preview = ''
+
+    await exec.exec('terraform', ['show', '-no-color', out], {
+      cwd,
+      listeners: {
+        stdout: (data: Buffer) => {
+          preview += data.toString()
+        },
+      },
+    })
+
     core.setOutput('plan', path.resolve(out))
+    core.setOutput('preview', `# Plan\n\n\`\`\`\n${preview.trim()}\n\`\`\``)
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
